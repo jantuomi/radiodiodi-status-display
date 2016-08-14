d3d9b6861dc5c6871ad138ab9f428bcdb1d61398
Jan Tuomi <jan-sebastian.tuomi@aalto.fi>
2016-08-14 13:16:10 +0300

    Add placeholder view and visuals, add express and d3

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var statuscake = require("statuscake");
- 
+var express = require("express");
+var ejs = require("ejs");
+
+var app = express();
+app.set("views", __dirname + "/views");
+app.engine("html", ejs.renderFile);
+app.use(express.static("public"));
+
 var args = process.argv.slice(2);
 if (args.length != 2) {
     console.log("Please provide user and key as positional arguments.");
@@ -13,6 +20,14 @@ statuscake
   .username(username)
   .key(password);
  
+app.get("/", function(req, res) {
+    res.render("index.html");
+});
+
+app.listen(3000, function() {
+    console.log("radiodiodi status display listening on port 3000.");
+});
+
 function allTests(err, data) {
     console.log(data);
 }
